f6941828697db6332ed2c0be0dfe1277325cf343
Malinali Becerril <mali@devf.mx>
2024-03-07 16:38:44 -0600

    fix(images): reorder exports to static configuration to firebase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white gray:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
             <Image
-              src="/logos/Logo.png"
+              src="logo.png"
               alt="Malinali Logo"
               width={224}
               height={161}
@@ -19,8 +19,8 @@ export default function Home() {
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/images/profile-avatar.png"
-          alt="Next.js Logo"
+          src="profile-avatar.png"
+          alt="Profile"
           width={180}
           height={37}
           priority
