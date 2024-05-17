a6dc88b6155335978bbea4f28e796ad290de7069
Malinali Becerril <mali@devf.mx>
2024-05-16 23:18:30 -0600

    fix(img): fix color in dark visualization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between px-24 py-8">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white gray:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white gray:from-black lg:static lg:h-auto lg:w-auto lg:bg-none">
             <Image
               src="/logo-1.png"
               alt="Malinali Logo"
